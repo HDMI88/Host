66046e9b141c1fd73371d6cf3901c159feddf149
HDMI88 <58102903+HDMI88@users.noreply.github.com>
2019-11-24 19:23:50 +0700

    Add files via upload

---
+++ encryptionHandler.js
@@ -0,0 +1,188 @@
+﻿function encryptionHandler() {
+
+
+
+    /*
+     PasswordDerivationOptions (options):
+     {
+        "DerivationType": "scrypt", // optionally: rfc
+        "Salt": "3a069e9126af66a839067f8a272081136d8ce63ed72176dc8a29973d2b15361f", //SALT must be in Hex
+        "Cost": 16384, //only for DerivationType "scrypt", not for "rfc"
+        "BlockSize": 8, //only for DerivationType "scrypt", not for "rfc"
+        "Parallel": 1, //only for DerivationType "scrypt", not for "rfc"
+        "KeySizeInBytes": 32,
+        "DerivationIterations": 0 // Only for DerivationType "rfc", not needed for "scrypt"
+
+    }
+    */
+
+    /*
+     * Simple text-based Encryption
+     */
+
+    this.encrypt = function (plainText, passPhrase, options) {
+        var that = this;
+        var rawPlainData;
+        options = autocompleteOptions(options);
+        var derivedKey = that.getDerivedKey(passPhrase, options);
+        rawPlainData = string2arraybuffer(plainText);
+        return webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, true);
+    }
+
+    /*
+     * Simple text-based Decryption
+     */
+
+    this.decrypt = function (encryptedData, passPhrase) {
+        var that = this;
+        var cO = JSON.parse(encryptedData);
+        var dKey = that.getDerivedKey(passPhrase, cO); //This key is in PBKDF2
+        var ciphertext = _base64ToArrayBuffer(cO.CipherOutputText);
+        return webcryptoBinaryDecrypt(ciphertext, dKey, cO, true);
+    }
+
+    /*
+    * Binary Decryption: rawEncryptedData must be in UintArray, passPhrase regular text
+     * returnUtf8Text: if true, it returns text (assuming, text was encrypted and not binary data)
+     * returnUtf8Text: if false, it returns binary
+    */
+
+    this.decryptBinary = function (rawEncryptedData, passPhrase, options, returnUtf8Text = false) {
+        var that = this;
+        var derivedKey = that.getDerivedKey(passPhrase, options);
+        return webcryptoBinaryDecrypt(rawEncryptedData, derivedKey, options, returnUtf8Text);
+    }
+
+    /*
+    * Binary Decryption with derived Key: as decryptBinary, but with derived key in Uint8array.
+     * This has the purpose that if deriving the key takes a lot of time and you have multiple
+     * files to decrypt, you just derive the key once and decrypt file by file without losing time.
+    */
+
+    this.decryptBinaryWithDerivedKey = function (rawEncryptedData, derivedKey, options, returnUtf8Text = false) {
+        return webcryptoBinaryDecrypt(rawEncryptedData, derivedKey, options, returnUtf8Text);
+    }
+
+    /*
+    * Binary Encryption with derived Key: Encrypt binary data
+    */
+
+    this.encryptBinary = function (rawPlainData, derivedKey, options, returnFullCryptoObject = false) {
+        options = autocompleteOptions(options);
+        return webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, returnFullCryptoObject);
+    }
+
+
+    // To manually derive a crypto key, e. g. a Scrypt-key
+    // Returns PBKDF2 key
+    //
+
+    this.getDerivedKey = function(passPhrase, options)
+    {
+        var cO = options;
+        //Encoding the Salt in from UTF8 to byte array
+        var Salt = CryptoJS.enc.Base64.parse(cO["Salt"]);
+        //Creating the Vector Key
+        var DerivedKey;
+
+        //Creating the key in PBKDF2 format to be used during the decryption
+        if (cO["DerivationType"] == "scrypt") {
+            var sc = new scryptHandler();
+            DerivedKey = hexStringToUint8Array(sc.GetOnlyHashInHexString(passPhrase, cO));
+        } else {
+            var Pass = CryptoJS.enc.Utf8.parse(passPhrase);
+            DerivedKey =
+                CryptoJS.PBKDF2(Pass.toString(CryptoJS.enc.Utf8), Salt, { keySize: cO["KeySizeInBytes"] * 8 / 32, iterations: cO["DerivationIterations"] });
+        }
+        return DerivedKey;
+    }
+
+
+    // return Utf8Text true = returns text, else returns binary
+    function webcryptoBinaryDecrypt(rawEncryptedData, derivedKey, options, returnUtf8Text = true) {
+        return new Promise(function (resolve, reject) {
+            var iv = _base64ToArrayBuffer(options.AesRijndaelIv);
+            crypto.subtle.importKey("raw", derivedKey, "AES-CBC", false, ["encrypt", 'decrypt'])
+                .then(function (key) {
+                    return crypto.subtle.decrypt({ 'name': 'AES-CBC', iv: iv }, key, rawEncryptedData);
+                },
+                    reject)
+                .then(function (plainText) {
+                    if (returnUtf8Text) {
+                        resolve(new TextDecoder("utf-8").decode(plainText));
+                    } else {
+                        resolve(plainText);
+                    }
+
+                },
+                    reject);
+        });
+
+    }
+
+
+     
+
+    // Send rawPlainData in Uint8Array!
+    function webcryptoBinaryEncrypt(rawPlainData, derivedKey, options, returnFullCryptoObject = false) {
+        return new Promise(function (resolve, reject) {
+            var iv = _base64ToArrayBuffer(options.AesRijndaelIv);
+            crypto.subtle.importKey("raw", derivedKey, 'AES-CBC', false, ["encrypt", 'decrypt'])
+                .then(function (key) {
+                    return crypto.subtle.encrypt({ 'name': 'AES-CBC', iv: iv }, key, rawPlainData);
+                    },
+                    reject)
+                .then(function (cipherText) {
+                    if (returnFullCryptoObject) {
+                            options.CipherOutputText = _arrayBufferToBase64(cipherText);
+                            resolve(JSON.stringify(options));
+                        } else {
+                            resolve(cipherText);
+                        }
+                    },
+                    reject);
+        });
+    }
+
+    function autocompleteOptions(options) {
+        var rijndaelIv;
+        var salt;
+        if (options == null || options.AesRijndaelIv == null || options.Salt == null) {
+            var array1 = new Uint8Array(16);
+            var array2 = new Uint8Array(32);
+            rijndaelIv = _arrayBufferToBase64(window.crypto.getRandomValues(array1));
+            salt = arrayBufferToHex(window.crypto.getRandomValues(array2));
+        
+        }
+        if (options == null) { // Scrypt is default (not rfc)
+            options = {
+                "DerivationType": "scrypt",
+                "Salt": salt, //(can be empty or null, then string is automatically created)
+                "Cost": 16384, //(the "N" of scrypt, default is 16384)
+                "BlockSize": 8, // (the "r", default is 8)
+                "Parallel": 1, // (the "p", default is 1)
+                "KeySizeInBytes": 32, // (default is 32),
+                "AesRijndaelIv": rijndaelIv
+            }
+        } else {
+            // if one of the options exist, but others not
+            ('DerivationType' in options) || (options.DerivationType = "scrypt");
+            ('Salt' in options) || (options.Salt = salt);
+            ('Cost' in options) || (options.Cost = 16384);
+            ('BlockSize' in options) || (options.BlockSize = 8);
+            ('Parallel' in options) || (options.Parallel = 1);
+            ('KeySizeInBytes' in options) || (options.KeySizeInBytes = 32);
+            ('DerivationIterations' in options) || (options.DerivationIterations = 10000);
+            ('AesRijndaelIv' in options) || (options.AesRijndaelIv = rijndaelIv);
+        }
+        return options;
+    }
+
+
+
+   
+}
+
+
+
+
