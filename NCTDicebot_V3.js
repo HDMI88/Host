9a845c0311d86ef3e24df8a11f68afdc00693373
HDMI88 <58102903+HDMI88@users.noreply.github.com>
2022-07-05 04:22:47 +0700

    Update NCTDicebot_V3.js

--- NCTDicebot_V3.js
+++ NCTDicebot_V3.js
@@ -1,11 +1,12 @@
 var NCTDicebot_ImportedJS_CSS;
 (async function()
 {
-    // Chạy hàm thêm các file JS và CSS cần thiết cho BOT vào trang web hiện tại
+    // Chạy hàm thêm các file JS và CSS cần thiết cho BOT vào trang web hiện tại 
     if (typeof jQuery == 'undefined') await ImportScript('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+    await ImportScript('https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js');
+    await ImportScript('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js');
     await ImportScript('https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js');
     await ImportScript('https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js');
-    await ImportScript('https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js');
     await ImportScript('https://firebasestorage.googleapis.com/v0/b/nct-dicebot-7b956.appspot.com/o/canvasjs.min.js?alt=media&token=234d6a1b-13b7-48f3-a40b-ca24a2ce4cc6');
     await ImportScript('https://firebasestorage.googleapis.com/v0/b/nct-dicebot-7b956.appspot.com/o/fengari-web.js?alt=media&token=e05a27e0-6ca1-40b9-aead-c68715a90b12');
     await ImportScript('https://firebasestorage.googleapis.com/v0/b/nct-dicebot-7b956.appspot.com/o/awesome-notifications.js?alt=media&token=7e3bc4bc-d6e4-4f51-a5ec-452e45cced54');
