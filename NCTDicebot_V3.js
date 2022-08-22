b3677b697f6d29cd7f700d0b340ab61f136fcf24
HDMI88 <58102903+HDMI88@users.noreply.github.com>
2022-08-22 23:30:10 +0700

    Update NCTDicebot_V3.js

--- NCTDicebot_V3.js
+++ NCTDicebot_V3.js
@@ -26,9 +26,9 @@
     <iframe id = "ADS02" data-aa="1738492" src="//ad.a-ads.com/1738492?size=728x90" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;" ></iframe>
     <iframe id = "WebsiteNCT01" src="https://www.ncttechnology.net" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;" ></iframe>
     `;
-    window.setInterval("ReloadIFrame('ADS01', '//ad.a-ads.com/1738316?size=728x90');", 60000);
-    window.setInterval("ReloadIFrame('ADS02', '//ad.a-ads.com/1738492?size=728x90');", 60000);
-    window.setInterval("ReloadIFrame('WebsiteNCT01', 'https://www.ncttechnology.net');", 60000);
+    window.setInterval("ReloadIFrame('ADS01', '//ad.a-ads.com/1738316?size=728x90');", 120000);
+    window.setInterval("ReloadIFrame('ADS02', '//ad.a-ads.com/1738492?size=728x90');", 120000);
+    window.setInterval("ReloadIFrame('WebsiteNCT01', 'https://www.ncttechnology.net');", 600000);
     
     if (stringIsEmpty(document.querySelector("#NCT_ADS01")) == true)
     {
